6cc0223f6d7864434997d630489e4bd759c8df12
noortxlib <116088887+noortxlib@users.noreply.github.com>
2023-02-23 14:47:59 +0000

    navbar

---
+++ Team-11/HTML/header.php
@@ -0,0 +1,37 @@
+<head>
+    <meta charset="utf-8" />
+    <title>Step Correct</title>
+    <link rel="icon" href="images/favicon.ico?v=2" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script src="https://kit.fontawesome.com/c54d8b2ca0.js" crossorigin="anonymous">
+    </script>
+    <script defer src="js/main.js"></script>
+</head>
+
+
+    <div class="topnav" id="myTopnav">
+
+        <div class="topnav-left">
+            <img src="images/logo.png" alt="" />
+        </div>
+
+        <div class="topnav-centre">
+            <a href="index.php">Home</a>
+            <a href="#">Womens</a>
+            <a href="#">Mens</a>
+            <a href="#">About</a>
+            <a href="#">Contact Us</a>
+        </div>
+
+        <div class="topnav-right">
+            <a href="#"><i class="fa fa-search"></i></a>
+            <a href="#"><i class="fas fa-user-circle"></i></a>
+            <a href="wishlist.php"><i class="fa fa-heart-o"></i></a>
+            <a href="basket.php"><i class="fa fa-shopping-cart"></i></a>
+        </div>
+
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+
+    </div>
