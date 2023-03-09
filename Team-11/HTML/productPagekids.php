34371b2682a279aa2ce158bec32a4064d1126128
ibrahim <116092627+ibby0011@users.noreply.github.com>
2023-03-09 20:54:39 +0000

    kids
    kids

--- Team-11/HTML/productPagekids.php
+++ Team-11/HTML/productPagekids.php
@@ -1,9 +1,14 @@
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL); 
+error_reporting(E_ALL);
 
 require_once 'connect.php';
+
+$category = "";
+if (isset($_GET['category'])) {
+    $category = $_GET['category'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,100 +18,223 @@ require_once 'connect.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Product Page</title>
-    <link rel = "stylesheet" href = "../css/productPage1.css">
+    <link rel = "stylesheet" href = "../CSS/productPage1.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../CSS/womensproducts.css">
+
 </head>
 
 <body>
-<ul class="navbar">
-        <li><a href="">Home</a></li>
+<?php include('header.php');  ?>
 
-        <li class="dropdown">
-            <a href="" class="dropbtn">Products</a>
-            <div class="dropdown-content">
-                <?php
-                include('connect.php');
-                
-                    $sql = "SELECT * FROM categories";
-                    $result = $db->query($sql);
-
-                    // Process the query results
-                    if ($result->num_rows > 0) {
-                        // Output data of each row
-                        while($row = $result->fetch_assoc()) {
-                            echo '<a href="">'.$row["category_title"].'</a>';
-                        }
-                    } else {
-                        echo "0 results";
-                    }
-                ?>
-            </div>
-        </li>
-   
-        <li><a href="">Contact</a></li>
-        <li><a href="">About</a></li>
-    </ul>
-
-    <div class = "wrapper">
-        <div class = "container">
+    <div class = "container">
+<main>
             <div class = "header">
-                <h1>Kids</h1>
+                <hr>
+                <h1>Shop Kids</h1>
+                <hr>
             </div>
+        
+    
+<?php if (!isset($_GET['category'])) { ?>
 
-
-            <div class="products">
-            <div class="product-container">
-                <?php
-                    include('connectprd.php');
-                    /*change the end id to change what products show (category_id)*/
-                    $sql = "SELECT * FROM products p JOIN categories c ON p.category_id = c.category_id WHERE c.category_id = '3'";
-                    
-                    $result = $db->query($sql);
-                    /*fetches the products specified by each category from database*/
-                    if ($result->num_rows > 0) {
-                        while ($row = $result->fetch_assoc()) {
-                            $image = base64_encode($row['product_image']);
-                            $title = $row['product_title'];
-                            $price = $row['product_price'];
-                            echo '<div class="product-item">
-                                        <div class="product-img">
-                                        <img src="data:image/jpg;base64,'.$image.'">
-                                            <div>
-                                                <button type="button" class="cart-btn">Add to Cart</button>
-                                            </div>
-                                        </div>
-                                        <div class="product-info">
-                                            <a href="#" class="item-name">'.$title.'</a>
-                                            <span class="price">£ '.$price.'</span>
-                                            <div>
-                                                <ul class="rating">
-                                                    <li><i class="fas fa-star"></i></li>
-                                                    <li><i class="fas fa-star"></i></li>
-                                                    <li><i class="fas fa-star"></i></li>
-                                                    <li><i class="fas fa-star"></i></li>
-                                                    <li><i class="fas fa-star-half-alt"></i></li>
-                                                    <li>(70 reviews)</li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>';
-
-                                }
-                            } else {
-                                echo "0 results";
-                            }
-                        ?>
-                        
-                </div>
-            </div>
-        </div>
+<div class="category">
+    <div class="category">
+        <img src="../Images/kidsboots.jpg" alt="boots" />
+        <button class="centered" onclick="document.location='productPageMen.php?category=12'">Kids Boots</button>
     </div>
+    <div class="category">
+        <img src="../Images/kidshightops.jpg" alt="high-tops" />
+        <button class="centered" onclick="document.location='productPageMen.php?category=13'">High Tops</button>
+    </div>
+    <div class="category">
+        <img src="../Images/kidsmartshoe.jpg" alt="kidsmartshoe" />
+        <button class="centered" onclick="document.location='productPageMen.php?category=14'">smart shoes</button>
+    </div>
+    <div class="category">
+        <img src="../Images/trainerskids.jpg" alt="trainers" />
+        <button class="centered" onclick="document.location='productPageMen.php?category=15'">Trainers</button>
+    </div>
+    <div class="category">
+        <img src="../Images/slipperskid.jpg" alt="slippers" />
+        <button class="centered" onclick="document.location='productPageMen.php?category=16'">Slippers</button>
+    </div>
+</div>
 
+<?php } ?>
 
-</body>
+        <?php if (isset($_GET['category'])) { ?>
+            <button class="back"><a href="productPageKids.php">Browse other products</a></button>
+        <?php } ?>
 
+        
+                <?php 
+                
+                if (isset($_GET['category'])) {
+                    if ($category == '12') {
+                        $query = "SELECT * FROM products WHERE category=$category";
+                        $result = $conn->query($query);
+                        
+
+    
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                                $name = $row['name'];
+                                $price = $row['price'];
+                                $image = $row['image'];
+                                $id = $row['id'];
+    
+                                echo "<div class='gallery'>
+                        <div class='products'>
+                            <img src='../Images/$image' alt='$name'>
+                            <h3>$name</h3>
+                            <h6>$price</h6>
+                            <button class='view' onclick=document.location='viewproducts.php?id=$id'>View Now</button>
+                        </div>
+                        </div>";
+                            }
+                        }
+                        }
+                        
+                    } 
+                            
+                        ?>
+
+                <?php 
+                
+                if (isset($_GET['category'])) {
+                    if ($category == '13') {
+                        $query = "SELECT * FROM products WHERE category=$category";
+                        $result = $conn->query($query);
+                        
+
+    
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                                $name = $row['name'];
+                                $price = $row['price'];
+                                $image = $row['image'];
+                                $id = $row['id'];
+    
+                                echo "<div class='gallery'>
+                        <div class='products'>
+                            <img src='../Images/$image' alt='$name'>
+                            <h3>$name</h3>
+                            <h6>$price</h6>
+                            <button class='view' onclick=document.location='viewproducts.php?id=$id'>View Now</button>
+                        </div>
+                        </div>";
+                            }
+                        }
+                        }
+                        
+                    } 
+                            
+                        ?>
+
+                <?php 
+                
+                if (isset($_GET['category'])) {
+                    if ($category == '14') {
+                        $query = "SELECT * FROM products WHERE category=$category";
+                        $result = $conn->query($query);
+                        
+
+    
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                                $name = $row['name'];
+                                $price = $row['price'];
+                                $image = $row['image'];
+                                $id = $row['id'];
+    
+                                echo "<div class='gallery'>
+                        <div class='products'>
+                            <img src='../Images/$image' alt='$name'>
+                            <h3>$name</h3>
+                            <h6>$price</h6>
+                            <button class='view' onclick=document.location='viewproducts.php?id=$id'>View Now</button>
+                        </div>
+                        </div>";
+                            }
+                        }
+                        }
+                        
+                    } 
+                            
+                        ?>
+
+                <?php 
+                
+                if (isset($_GET['category'])) {
+                    if ($category == '15') {
+                        $query = "SELECT * FROM products WHERE category=$category";
+                        $result = $conn->query($query);
+                        
+
+    
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                                $name = $row['name'];
+                                $price = $row['price'];
+                                $image = $row['image'];
+                                $id = $row['id'];
+    
+                                echo "<div class='gallery'>
+                        <div class='products'>
+                            <img src='../Images/$image' alt='$name'>
+                            <h3>$name</h3>
+                            <h6>$price</h6>
+                            <button class='view' onclick=document.location='viewproducts.php?id=$id'>View Now</button>
+                        </div>
+                        </div>";
+                            }
+                        }
+                        
+                        }
+                    } 
+                            
+                        ?>
+
+                <?php 
+                
+                if (isset($_GET['category'])) {
+                    if ($category == '16') {
+                        $query = "SELECT * FROM products WHERE category=$category";
+                        $result = $conn->query($query);
+                        
+
+    
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                                $name = $row['name'];
+                                $price = $row['price'];
+                                $image = $row['image'];
+                                $id = $row['id'];
+    
+                                echo "<div class='gallery'>
+                        <div class='products'>
+                            <img src='../Images/$image' alt='$name'>
+                            <h3>$name</h3>
+                            <h6>$price</h6>
+                            <button class='view' onclick=document.location='viewproducts.php?id=$id'>View Now</button>
+                        </div>
+                        </div>";
+                            }
+                            
+                        }
+                        
+                    }
+                    } 
+                            
+                        ?>
+                         
+                
+
+        </div>
+            
+                    <main>
+        </body>
+
+        
 </html>
-
-
-
-</body>
