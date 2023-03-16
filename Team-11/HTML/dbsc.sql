33f3f0f3e58bf98fe39ffa42a51af35ec2fad9e9
noortxlib <116088887+noortxlib@users.noreply.github.com>
2023-03-16 14:36:53 +0000

    Update dbsc.sql

--- Team-11/HTML/dbsc.sql
+++ Team-11/HTML/dbsc.sql
@@ -30,7 +30,9 @@ CREATE TABLE `products` (
   `category` int (5) COLLATE utf8mb4_unicode_ci NOT NULL,
   `quantity` int(5) COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` int(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `image` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL
+  `image` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image1` varchar(500) NOT NULL,
+  `image2` varchar(500) NOT NULL
   
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci; 
 
@@ -82,9 +84,12 @@ CREATE TABLE `orders` (
 );
 --block 4 end--
 --insert products into database--
-INSERT INTO `products` (`id`, `name`, `description`, `category`, `quantity`, `price`, `image`) VALUES
-(2, 'Black heal', 'Elegant strap', 2, 30, 20, 'blackheal.jpg'),
-(3, 'Red heal', 'Strap', 2, 20, 20, 'redheal.jpg'),
-(4, 'Black boots', 'Everyday boots', 1, 10, 26, 'blackboots.jpg'),
-(5, 'Fluffy slippers', 'Casual', 5, 20, 11, 'fluffyslippers.jpg'),
-(7, 'Black nike trainers', '', 4, 0, 70.99, 'niketrainers.jpg');
+INSERT INTO `products` (`id`, `name`, `description`, `category`, `quantity`, `price`, `image`, `image1`, `image2`) VALUES
+(4, 'Dr. Martens', 'Durable, classic boots.\r\n\r\n- Colour: black\r\n- 2.8cm heel\r\n- Upper Material: Leather\r\n- Lining: Leather\r\n- Sole: Rubber', 1, 10, 160, 'blackboots.jpg', 'blackboots1.jpg', 'blackboots2.jpg'),
+(5, 'Fluffy slippers', 'Casual', 5, 20, 11, 'fluffyslippers.jpg', '', ''),
+(8, 'Classic block heal', 'This wide fit heel is perfect for a night out, with an adjustable strap to fit you with absolute comfort. The high block heel fits perfectly with skinny jeans and various dress types. An open toe design provides any wearer with comfort and style. \r\nColour: Black \r\nHeel height: 8.5cm \r\nPin-buckle fastening ', 2, 30, 29.99, 'blackblockheal.jpg', 'blackblockheal1.jpg', 'blackblockheal2.jpg'),
+(9, 'Stilleto heels ', 'This flat soled heel provides the wearer with optimum comfort, combined with the pointed toe gives any wearer a sleek and stylish look. Suited for various dress types for a normal day as well as providing a formal business look. \r\n\r\n- Colour – black  \r\n- Heel height: 7.5cm \r\n- Mid point heel \r\n ', 2, 20, 32.99, 'stilettohealsblack.jpg', 'stilettohealsblack1.jpg', 'stilettohealsblack2.jpg'),
+(10, 'Gold heel sandals', 'With the heels gold coloured, shine bright in any place these are worn. It is flashy and subtle, being the perfect shoe for any wearer. It is open toed providing an accent to whatever you choose to wear with this.\r\nColour: gold \r\nHeel height: 10cm \r\nHigh point heel \r\nAdjustable strap  ', 2, 10, 19.99, 'goldheal.jpg', 'goldheal1.jpg', 'goldheal2.jpg'),
+(11, 'Nike Metcon ', 'With a black and white colourway, these trainers provide a sleek look matching any outfit. They are tested for optimum running capabilities through the wide base which provides stability. A tough rubber outsole keeps the wearers feet gripped.\r\n \r\n- Colour: black and white \r\n- Material: textile & synthetic upper/synthetic sole \r\n- Textured overlays  \r\n- Breathable mesh ', 4, 15, 115, 'nikemetcon.jpg', 'nikemetcon1.jpg', 'nikemetcon2.jpg'),
+(12, 'Adidas Originals', 'With a retro runner style, look sleek and stylish while using these running trainers to achieve higher speeds. These trainers are lightweight and breathable, which is helpful in running more comfortably. Includes plush padding around the ankles. \r\n\r\n- Colour – white tint \r\n- Locked in finish \r\n- Cushioned step ', 4, 5, 100, 'adidasoriginalsw.jpg', 'adidasoriginalsw1.jpg', 'adidasoriginalsw2.jpg');
+
