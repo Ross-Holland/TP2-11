18e18c02a5879aa5ff7c0e310d2761f1bd3b8a88
noortxlib <116088887+noortxlib@users.noreply.github.com>
2023-03-28 23:31:55 +0100

    search products by price and newest

--- Team-11/HTML/search.php
+++ Team-11/HTML/search.php
@@ -3,7 +3,7 @@
 
 <head>
     <link rel="stylesheet" type="text/css" href="../CSS/wishlist-basket.css" />
-    <link rel="stylesheet" href="../CSS/womensproducts.css">
+    <link rel="stylesheet" href="../CSS/products.css">
 </head>
 
 <body>
@@ -12,23 +12,52 @@
     require_once('connect.php');
 
     if (isset($_POST['search'])) {
-        $valueToSearch = $_POST['valueToSearch'];
-        $query = "SELECT * FROM products WHERE CONCAT(name, image) LIKE '%$valueToSearch%'";
-        $result = $conn->query($query);
+        $valueToSearch = $_POST['valueToSearch']; {
+            if (isset($_POST['LowToHigh'])) {
+                $query = "SELECT * FROM products WHERE CONCAT(name, image, description, price) LIKE '%$valueToSearch%' ORDER BY price ASC";
+                $result = $conn->query($query);
+            } else if (isset($_POST['HighToLow'])) {
+                $query = "SELECT * FROM products WHERE CONCAT(name, image, description, price) LIKE '%$valueToSearch%' ORDER BY price DESC";
+                $result = $conn->query($query);
+            } else if (isset($_POST['Newest'])) {
+                $query = "SELECT * FROM products WHERE CONCAT(name, image, description, price) LIKE '%$valueToSearch%' ORDER BY id DESC";
+                $result = $conn->query($query);
+            } else {
+                $query = "SELECT * FROM products WHERE CONCAT(name, image, description, price) LIKE '%$valueToSearch%'";
+                $result = $conn->query($query);
+            }
+        }
     } else {
-        $query = "SELECT * FROM products";
-        $result = $conn->query($query);
+        if (isset($_POST['LowToHigh'])) {
+            $query = "SELECT * FROM products ORDER BY price ASC";
+            $result = $conn->query($query);
+        } else if (isset($_POST['HighToLow'])) {
+            $query = "SELECT * FROM products ORDER BY price DESC";
+            $result = $conn->query($query);
+        } else if (isset($_POST['Newest'])) {
+            $query = "SELECT * FROM products  ORDER BY id DESC";
+            $result = $conn->query($query);
+        } else {
+            $query = "SELECT * FROM products";
+            $result = $conn->query($query);
+        }
     }
+
 
     ?>
 
     <div class="content">
         <main>
             <div class="searchprodcuts">
-                <h4>Search products</h4>
+                <h6>Search products</h6><br>
                 <form action="" method="post">
-                    <input type="text" name="valueToSearch" placeholder="Search" size="37"><br><br>
-                    <input type="submit" name="search" value="Search"><br><br>
+                    <input type="text" name="valueToSearch" placeholder="Search..." size="50"><br><br>
+                    <input type="radio" name="Newest" value="Newest"> Newest
+                    <input type="radio" name="LowToHigh" value="Price: Low-High "> Price: Low-High
+                    <input type="radio" name="HighToLow" value="Price: High-Low"> Price: High-Low
+                    <br><br>
+                    <input type="submit" name="search" class="filter" value="Filter">
+
                 </form>
             </div>
 
