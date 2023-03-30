bf4c158bdc2f5f416b5ee772c8bb73c2774bde69
sy3dahm3d <210055728@aston.ac.uk>
2023-03-30 11:43:27 +0100

    Update footer.php

--- Team-11/HTML/footer.php
+++ Team-11/HTML/footer.php
@@ -2,5 +2,6 @@
 <div class="footer">
   &copy <?php echo date('Y') ?> Step Correct LtD
   United Kingdom | GBP (£)
+  <br>This website has been developed for educational purposes only, and not for commercial purposes, some images used here have been extracted from commercial websites and brands including Nike, Adidas, Lacoste, Doc Martens, Diesel and some other brands.  We acknowledge this and plan to remove the images from the website after the completion of this project.
 </div>
 </footer
